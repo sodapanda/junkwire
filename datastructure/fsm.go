d1fe100360580288234d9794f81e611eedfffdf3
sodapanda <sodapanda20@gmail.com>
2020-09-06 02:24:10 +0800

    [fix]fix fsm bug

--- datastructure/fsm.go
+++ datastructure/fsm.go
@@ -4,15 +4,19 @@ import "fmt"
 
 //State 状态
 type State struct {
-	name   string
-	trans  map[Event]string //key是event value是state的name
-	action func(Event)
+	name  string
+	trans map[string]trans //key是event value是state的name
 }
 
 //Event trans event
 type Event struct {
 	Name       string
 	ConnPacket interface{}
+}
+
+type trans struct {
+	name   string      //to state
+	action func(Event) //action after trans
 }
 
 //Fsm 状态机
@@ -34,21 +38,24 @@ func (m *Fsm) AddRule(state string, event Event, toState string, action func(Eve
 	if m.states[state] == nil {
 		m.states[state] = new(State)
 		m.states[state].name = state
-		m.states[state].trans = make(map[Event]string)
+		m.states[state].trans = make(map[string]trans)
 	}
-	m.states[state].trans[event] = toState
-	m.states[state].action = action
+	m.states[state].trans[event.Name] = trans{name: toState, action: action}
 }
 
 //OnEvent 事件发生的回调
 func (m *Fsm) OnEvent(event Event) {
 	currentState := m.states[m.current]
-	nextStateName, ok := currentState.trans[event]
+	nextTrans, ok := currentState.trans[event.Name]
 	if !ok {
-		fmt.Printf("stata:%s has no event %s\n", currentState.name, event)
+		eventName := event.Name
+		if eventName == "" {
+			eventName = "nil"
+		}
+		fmt.Printf("stata:%s has no event %s\n", currentState.name, eventName)
 		return
 	}
-	nextState := m.states[nextStateName]
+	nextStateName := nextTrans.name
 	m.current = nextStateName
-	nextState.action(event)
+	nextTrans.action(event)
 }
